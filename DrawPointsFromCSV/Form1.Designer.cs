651dc635734669c45875173836e0352afb41ec5c
reinholtk24 <reinholtk24@mycu.concord.edu>
2017-09-03 19:07:55 -0900

    Read Coordinates from CSV and DrawLines

---
+++ DrawPointsFromCSV/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace DrawPointsFromCSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawingArea = new System.Windows.Forms.PictureBox();
+            this.drawButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.drawingArea)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // drawingArea
+            // 
+            this.drawingArea.Location = new System.Drawing.Point(85, 44);
+            this.drawingArea.Name = "drawingArea";
+            this.drawingArea.Size = new System.Drawing.Size(176, 163);
+            this.drawingArea.TabIndex = 0;
+            this.drawingArea.TabStop = false;
+            // 
+            // drawButton
+            // 
+            this.drawButton.Location = new System.Drawing.Point(4, 60);
+            this.drawButton.Name = "drawButton";
+            this.drawButton.Size = new System.Drawing.Size(75, 23);
+            this.drawButton.TabIndex = 1;
+            this.drawButton.Text = "Draw";
+            this.drawButton.UseVisualStyleBackColor = true;
+            this.drawButton.Click += new System.EventHandler(this.drawButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.drawButton);
+            this.Controls.Add(this.drawingArea);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.drawingArea)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox drawingArea;
+        private System.Windows.Forms.Button drawButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DrawPointsFromCSV/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawPointsFromCSV
+{
+    public partial class Form1 : Form
+    {
+        Graphics drawArea;
+        List<int> points= new List<int>(); 
+
+        public Form1()
+        {
+            InitializeComponent();
+            drawArea = drawingArea.CreateGraphics();
+            getPoints(); 
+        }
+
+
+        public void getPoints() // Reads in a txt file or csv file (Change file path in stream reader line 33), Then it converts each number to an int 
+                                // and adds the number to a list. The list of points called -> points, is later used to draw lines. 
+        {
+
+            int counter = 0;
+            string line;
+
+            // Read the file and display it line by line.
+            System.IO.StreamReader file = new System.IO.StreamReader(@"c:\Users\Gon Freecs\points.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                line.Trim(); 
+                string[] lines = line.Split(',');
+
+                int max = lines.Length;
+
+                for (int i = 0; i < max; i++ )
+                {
+                    int tempPoint; 
+                    bool result = Int32.TryParse(lines[i], out tempPoint);
+                    
+                    if(result)
+                    {
+                        points.Add(tempPoint);
+                    }
+
+                }
+            }
+
+            file.Close();
+            
+        }
+        private void drawButton_Click(object sender, EventArgs e)
+        {
+            Pen blackPen = new Pen(Color.Black, 3);
+
+            for(int j = 0; j < points.Count; j++)
+            {
+                if(j + 3 == points.Count)
+                {
+                    drawArea.DrawLine(blackPen, points.ElementAt(j-1), points.ElementAt(j), points.ElementAt(j + 1), points.ElementAt(j +2)); 
+
+                    break; 
+                }
+                else
+                {
+                    drawArea.DrawLine(blackPen, points.ElementAt(j), points.ElementAt(j + 1), points.ElementAt(j + 2), points.ElementAt(j + 3));
+                }
+
+            }
+
+        }
+    }
+}
