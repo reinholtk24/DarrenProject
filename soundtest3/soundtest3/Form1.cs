271aeb8d18fe1b221d85d0af9b1068bafd8b7262
reinholtk24 <reinholtk24@mycu.concord.edu>
2017-09-05 17:29:00 -0900

    Play separate sound/video file simultaneously

---
+++ soundtest3/soundtest3/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib; 
+
+namespace soundtest3
+{
+    public partial class Form1 : Form
+    {
+        WindowsMediaPlayer myPlayer = new WindowsMediaPlayer(); 
+        public Form1()
+        {
+            InitializeComponent();
+            myPlayer.URL = "Recording.m4a";
+            sync(); 
+        }
+
+        public void sync()
+        {
+            myPlayer.controls.play(); 
+        }
+    }
+}
